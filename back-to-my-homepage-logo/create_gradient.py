b2cf9b48b868a934bf292ecfc49248edb7f5a455
Shlomi Fish <shlomif@shlomifish.org>
2013-05-13 23:08:02 +0300

    Fix the create_gradient script.

--- back-to-my-homepage-logo/create_gradient.py
+++ back-to-my-homepage-logo/create_gradient.py
@@ -3,9 +3,19 @@
 import math
 from gimpfu import *
 
+def is_str_in_tuple(s, t):
+    for x in t:
+        if (x == s):
+            return True
+    return False
+
 def create_bk2hp_gradient():
-    req_grad_name = 'Back to My Homepage Gradient'
-    pdb.gimp_gradient_delete( req_grad_name )
+    req_grad_name = 'Back-to-My-Homepage-Gradient'
+
+    existing_gradients = pdb.gimp_gradients_get_list(req_grad_name)
+    if is_str_in_tuple(req_grad_name, existing_gradients[1]):
+        pdb.gimp_gradient_delete( req_grad_name )
+
     name = pdb.gimp_gradient_new( req_grad_name )
     pdb.gimp_gradient_segment_range_split_uniform( name, 0, 0, 2)
 
@@ -19,17 +29,20 @@ def create_bk2hp_gradient():
     return
 
 register(
-        "create_bk2hp_gradient",
-        "Create the Back to My Homepage Gradeint",
-        "Create the Back to My Homepage Gradeint",
-        "Shlomi Fish",
-        "Shlomi Fish",
-        "2013",
-        "<Image>/File/Create/Gradients/Back to my Homepage",
-        "RGB*, GRAY*",
-        [
-        ],
-        [],
-        create_bk2hp_gradient)
+    "create_bk2hp_gradient",
+    "Create the Back to My Homepage Gradeint",
+    "Create the Back to My Homepage Gradeint",
+    "Shlomi Fish",
+    "Shlomi Fish",
+    "2013",
+    N_("Create Gradient..."),
+    "",
+    [
+    ],
+    [],
+    create_bk2hp_gradient,
+    menu="<Image>/File/Create/Gradients/Back to my Homepage",
+    domain=("gimp20-python", gimp.locale_directory)
+)
 
 main()
