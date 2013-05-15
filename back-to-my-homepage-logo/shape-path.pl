13cd2694abec3bee7d4a590bf1dd6a2041a45771
Shlomi Fish <shlomif@shlomifish.org>
2013-05-15 14:57:06 +0300

    Add the path-union.

--- back-to-my-homepage-logo/shape-path.pl
+++ back-to-my-homepage-logo/shape-path.pl
@@ -5,12 +5,17 @@ use warnings;
 
 use XML::LibXML;
 
+use Image::SVG::Path 'extract_path_info';
+
 my $dom = XML::LibXML->load_xml(
     location => './back-to-my-homepage-slanted-path-union.svg'
 );
 my $xpc = XML::LibXML::XPathContext->new($dom);
 $xpc->registerNs('svg' => 'http://www.w3.org/2000/svg');
 
-my ($node) = $xpc->findnodes('//svg:path');
+my ($path_el) = $xpc->findnodes('//svg:path');
 
-print $node->toString(), "\n";
+my 
+# print $path_el->toString(), "\n";
+
+
