7e6c96a307289864c721b418df8c5da29da4509c
Shlomi Fish <shlomif@shlomifish.org>
2020-07-25 15:15:25 +0300

    fixes and cleanups

--- back-to-my-homepage-logo/scripts/shlomif_back_to_homepage_distort.py
+++ back-to-my-homepage-logo/scripts/shlomif_back_to_homepage_distort.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 '''
 Back to Homepage Distort
 
@@ -35,65 +35,79 @@ under ~/.config/inkscape/extensions
 
 '''
 
-import inkex, os, simplepath, cubicsuperpath, simpletransform, sys
-import simplestyle
-import tempfile
 import shutil
 import subprocess
-import string
-from ffgeom import *
-
+import sys
+import tempfile
 from os.path import expanduser, join
+
+import inkex
+
+from lxml import etree
+
+# from ffgeom import *
 
 temp_dir = tempfile.mkdtemp()
 
+
 def param(name):
-    Params = {
+    _params = {
             'distort_wh_ratio': 3,
             'bez_w_percent': 20,
             'bez_h_percent': 20,
             'perspective_wh_ratio': 2,
             'perspective_edge_percent': 50,
     }
-    ret = Params[name]
-    if (isinstance(ret, (int, long))):
+    ret = _params[name]
+    if (isinstance(ret, int)):
         return float(ret)
     else:
         return ret
+
 
 def temp_svg_fn(basename):
     global temp_dir
     return join(temp_dir, basename + '.svg')
 
-def p2s(x,y):
+
+def p2s(x, y):
     return str(x) + ',' + str(y)
 
+
 def draw_generic_style_path(name, my_id, parent, d_s):
-    style = {   'stroke'        : '#000000',
-                'stroke-width'  : '1',
-                'fill'          : 'none'            }
+    style = {
+            'stroke': '#000000',
+            'stroke-width': '1',
+            'fill': 'none'
+            }
 
-    path_attribs = {'style' : simplestyle.formatStyle(style),
-                    inkex.addNS('label','inkscape') : name,
+    path_attribs = {'style': str(inkex.Style(style)),
+                    inkex.addNS('label', 'inkscape'): name,
                     'id': my_id,
-                    'd' : d_s}
+                    'd': d_s}
 
-    path = inkex.etree.SubElement(parent, inkex.addNS('path','svg'), path_attribs )
+    path = etree.SubElement(
+        parent, inkex.addNS('path', 'svg'), path_attribs)
 
     return path
 
-def calc_distort_path((x,y), (w,h), bez_w_percent, bez_h_percent):
+
+def calc_distort_path(xy, wh, bez_w_percent, bez_h_percent):
+    x, y = xy
+    w, h = wh
     bez_w = (h * bez_w_percent) / 100
     bez_h = (h * bez_h_percent) / 100
     d_s = 'm '
-    d_s += p2s(x,y) + ' '
-    d_s += p2s(w,0) + ' '
-    d_s += p2s(0,h) + ' '
-    d_s += p2s(-w,0) + ' '
-    d_s += 'c ' + p2s(bez_w,-bez_h) + ' ' + p2s(bez_w,-(h-bez_h)) + ' ' + p2s(0,-h) + ' '
+    d_s += p2s(x, y) + ' '
+    d_s += p2s(w, 0) + ' '
+    d_s += p2s(0, h) + ' '
+    d_s += p2s(-w, 0) + ' '
+    d_s += 'c ' + p2s(bez_w, -bez_h) + ' ' + \
+        p2s(bez_w, -(h-bez_h)) + ' ' + p2s(0, -h) + ' '
     d_s += 'z'
 
-    return d_s;
+    return d_s
+
 
 class GenericAddPathEffect(inkex.Effect):
 
@@ -103,7 +117,9 @@ class GenericAddPathEffect(inkex.Effect):
         inkex.Effect.__init__(self)
 
     def effect(self):
-        draw_generic_style_path(self.get_path_name(), self.get_path_id(), self.current_layer,
+        draw_generic_style_path(
+            self.get_path_name(), self.get_path_id(),
+            self.svg.get_current_layer(),
             self.calc_d_s()
         )
 
@@ -111,8 +127,8 @@ class GenericAddPathEffect(inkex.Effect):
         return temp_svg_fn(self.basename)
 
     def write_to_temp(self):
-        self.affect(args=[self.input_fn], output=False)
-        with open(self.calc_out_fn(), 'w') as fh:
+        self.run(args=[self.input_fn], output=False)
+        with open(self.calc_out_fn(), 'wb') as fh:
             self.document.write(fh)
 
         return
@@ -135,11 +151,13 @@ class AddDistortPathEffect(GenericAddPathEffect):
             param('bez_w_percent'), param('bez_h_percent')
         )
 
-def calc_perspective_path( p1, p2, p3, p4):
-    d_list = ['M'] + [p2s(*p) for p in [p1,p2,p3,p4]] + ['z']
-    d_s = string.join(d_list, ' ')
+
+def calc_perspective_path(p1, p2, p3, p4):
+    d_list = ['M'] + [p2s(*p) for p in [p1, p2, p3, p4]] + ['z']
+    d_s = ' '.join(d_list)
 
     return d_s
+
 
 class AddPerspectivePathEffect(GenericAddPathEffect):
 
@@ -160,16 +178,21 @@ class AddPerspectivePathEffect(GenericAddPathEffect):
         edge_h = (half_h * param('perspective_edge_percent')) / 100
 
         return calc_perspective_path(
-            (orig_x,orig_y),
-            (orig_x,orig_y-h),
-            (orig_x+w,orig_y-h+edge_h),
-            (orig_x+w,orig_y-edge_h)
+            (orig_x, orig_y),
+            (orig_x, orig_y-h),
+            (orig_x+w, orig_y-h+edge_h),
+            (orig_x+w, orig_y-edge_h)
         )
+
 
 class StyleEffect(GenericAddPathEffect):
     def effect(self):
-        self.xpathSingle(u'svg:defs').insert(0,
-                inkex.etree.XML('''<svg:linearGradient xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" id="bk2hp_grad">
+        svg_start = '<svg:linearGradient ' + \
+            'xmlns:svg="http://www.w3.org/2000/svg"' + \
+            ' xmlns="http://www.w3.org/2000/svg" id="bk2hp_grad">'
+        self.svg.getElement(u'svg:defs').insert(
+            0,
+            etree.XML('''{svg_start}
                 <stop style="stop-color:#0000ff;stop-opacity:1"
                     offset="0"
                     id="b2h_0" />
@@ -182,56 +205,73 @@ class StyleEffect(GenericAddPathEffect):
                 <stop style="stop-color:#0000ff;stop-opacity:1"
                     offset="1"
                     id="b2h_3" />
-                </svg:linearGradient>''')
+                </svg:linearGradient>'''.format(svg_start=svg_start))
                 )
 
         # Query the bounding box of main_path_id()
         q = {'x': 0, 'y': 0, 'width': 0, 'height': 0}
         for param in q.keys():
             param_val_from_input = subprocess.check_output(
-                ['inkscape', ('--query-'+param), ('--query-id=' + main_path_id()), self.input_fn]
+                ['inkscape', ('--query-'+param),
+                 ('--query-id=' + main_path_id()), self.input_fn]
             )
             q[param] = float(param_val_from_input)
 
-        self.xpathSingle(u'svg:defs').insert(1,
-                inkex.etree.XML('''<svg:linearGradient xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="bk2hp_grad_rot" xlink:href="#bk2hp_grad" x1="%(x)f" y1="%(start_y)f" x2="%(x)f" y2="%(end_y)f" gradientUnits="userSpaceOnUse" />''' % {
-                        'x': (q['x'] + q['width']*0.5),
-                        'start_y': q['y'],
-                        'end_y': (q['y']+q['height']),
-                    })
-                )
-        self.xpathSingle(u'//*[@id="' + main_path_id() + '"]').set(
-                'style', simplestyle.formatStyle({
+        svg_start = \
+            '<svg:linearGradient xmlns:svg="http://www.w3.org/2000/svg" ' + \
+            'xmlns="http://www.w3.org/2000/svg" ' + \
+            'xmlns:xlink="http://www.w3.org/1999/xlink" id="bk2hp_grad_rot"'
+        self.svg.getElement(u'svg:defs').insert(
+            1,
+            etree.XML(
+                ('''%(svg_start)s xlink:href="#bk2hp_grad" x1="%(x)f" ''' +
+                 '''y1="%(start_y)f" x2="%(x)f" y2="%(end_y)f" ''' +
+                 '''gradientUnits="userSpaceOnUse" />''')
+                % {
+                    'x': (q['x'] + q['width']*0.5),
+                    'svg_start': svg_start,
+                    'start_y': q['y'],
+                    'end_y': (q['y']+q['height']),
+                })
+        )
+        self.svg.getElement(u'//*[@id="' + main_path_id() + '"]').set(
+                'style', str(inkex.Style({
                     'stroke': '#888888',
                     'stroke-width': '1pt',
                     'fill': 'url(#bk2hp_grad_rot)',
                     }
-                    ))
+                    )))
+
 
 def main_path_id():
     return 'back'
 
+
 def id_arg(my_id):
     return '--id=' + my_id
+
 
 distort_e = AddDistortPathEffect('with_path', sys.argv[-1])
 distort_e.write_to_temp()
 
 with_envelope_text = subprocess.check_output(
-        ['python',
-            join(expanduser('~'), '.config', 'inkscape', 'extensions', 'bezierenvelope.py'),
+        [
+            'python',
+            join(expanduser('~'), '.config', 'inkscape',
+                 'extensions', 'bezierenvelope.py'),
             id_arg(main_path_id()),
             id_arg(distort_e.get_path_id()),
             distort_e.calc_out_fn()
         ]
 )
 
-with_envelope__filename = temp_svg_fn('with_envelope');
+with_envelope__filename = temp_svg_fn('with_envelope')
 
-with open(with_envelope__filename, 'w') as fh:
+with open(with_envelope__filename, 'wb') as fh:
     fh.write(with_envelope_text)
 
-pers_e = AddPerspectivePathEffect('with_persepctive_path',
+pers_e = AddPerspectivePathEffect(
+    'with_persepctive_path',
     with_envelope__filename
 )
 pers_e.write_to_temp()
@@ -239,7 +279,8 @@ pers_e.write_to_temp()
 # with_pers_path__text = open(with_pers_path__filename).read()
 
 with_perspective_applied_text = subprocess.check_output(
-        ['python',
+        [
+            'python',
             '/usr/share/inkscape/extensions/perspective.py',
             id_arg(main_path_id()),
             id_arg(pers_e.get_path_id()),
@@ -247,14 +288,15 @@ with_perspective_applied_text = subprocess.check_output(
         ]
 )
 
-with_perspective_applied__filename = temp_svg_fn('with_pers_applied');
+with_perspective_applied__filename = temp_svg_fn('with_pers_applied')
 
-with open(with_perspective_applied__filename, 'w') as fh:
+with open(with_perspective_applied__filename, 'wb') as fh:
     fh.write(with_perspective_applied_text)
 
 style_e = StyleEffect('after_styling', with_perspective_applied__filename)
-style_e.write_to_temp()
-
-sys.stdout.write(open(style_e.calc_out_fn()).read())
+style_e.run()
+# style_e.write_to_temp()
+# with open(style_e.calc_out_fn(), 'rt') as fh:
+#     sys.stdout.write(fh.read())
 
 shutil.rmtree(temp_dir)
